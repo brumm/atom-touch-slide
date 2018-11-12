3dac489c47801d82fb4cfc03aaa078f123b67398
Philipp Brumm <philipp.brumm@gmail.com>
2018-11-12 22:55:14 +0100

    get outta here! this works??

---
+++ README.md
@@ -0,0 +1,5 @@
+# atom-touch-slide
+
+_This is experimental and silly. Use at your own risk!_
+
+Use your TouchBar™ to move your cursor on the current line.
---
+++ spec/touch-slide-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TouchSlideView from '../lib/touch-slide-view';
+
+describe('TouchSlideView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
